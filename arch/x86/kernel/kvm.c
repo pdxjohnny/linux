9f4eb7e98494cc370e05c12bf1dc2c5692afdfb9
John Andersen <john.s.andersen@intel.com>
2019-10-09 20:18:20 -0400

    kvm: Use MSR_KVM_CR4_NO_DISABLE
    Example kernel log from before MSR implemented in host kvm module:
    
    [    0.117316] KVM setup async PF for cpu 0
    [    0.117320] kvm-stealtime: cpu 0, msr 782b040
    [    0.117321] Enable
    [    0.117322] CR4 read     : 606b0
    [    0.117322] CR4 write    : 3606b0
    [    0.117327] CR4 read back: 3606b0
    [    0.117328] Disable
    [    0.117328] CR4 read     : 3606b0
    [    0.117329] CR4 write    : 606b0
    [    0.117334] CR4 read back: 606b0
    [    0.117334] Re-enable
    [    0.117335] CR4 read     : 606b0
    [    0.117335] CR4 write    : 3606b0
    [    0.117338] CR4 read back: 3606b0
    [    0.117338] Protect      : 300000
    [    0.117348] unchecked MSR access error: WRMSR to 0x4b564d06 (tried to write 0x0000000000300000) at rIP)
    [    0.117349] Call Trace:
    [    0.117352]  kvm_guest_cpu_init.cold+0x105/0x158
    [    0.117356]  kvm_smp_prepare_boot_cpu+0x19/0x95
    [    0.117358]  start_kernel+0x151/0x557
    [    0.117360]  secondary_startup_64+0xa4/0xb0
    [    0.117362] Try Disable
    [    0.117363] CR4 read     : 3606b0
    [    0.117363] CR4 write    : 606b0
    [    0.117367] CR4 read back: 606b0
    [    0.117370] Built 1 zonelists, mobility grouping on.  Total pages: 32105
    [    0.117371] Policy zone: DMA32
    [    0.117373] Kernel command line: console=ttyS0
    
    Signed-off-by: John Andersen <john.s.andersen@intel.com>

--- arch/x86/kernel/kvm.c
+++ arch/x86/kernel/kvm.c
@@ -369,6 +369,18 @@ static void kvm_guest_cpu_init(void)
 	native_write_cr4(cr4);
 	cr4 = native_read_cr4();
 	printk(pr_fmt("CR4 read back: %lx\n"), cr4);
+
+	printk(pr_fmt("Protect      : %lx\n"), (X86_CR4_SMEP | X86_CR4_SMAP));
+	wrmsrl(MSR_KVM_CR4_NO_DISABLE, (X86_CR4_SMEP | X86_CR4_SMAP));
+
+	printk(pr_fmt("Try Disable\n"));
+	cr4 = native_read_cr4();
+	printk(pr_fmt("CR4 read     : %lx\n"), cr4);
+	cr4 &= ~(X86_CR4_SMEP | X86_CR4_SMAP);
+	printk(pr_fmt("CR4 write    : %lx\n"), cr4);
+	native_write_cr4(cr4);
+	cr4 = native_read_cr4();
+	printk(pr_fmt("CR4 read back: %lx\n"), cr4);
 }
 
 static void kvm_pv_disable_apf(void)
