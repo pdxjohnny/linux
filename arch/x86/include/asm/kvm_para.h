81df84f80ea216a51403f1d659714cfa9bd520e9
John Andersen <john.s.andersen@intel.com>
2019-11-12 17:28:04 -0500

    X86: Use KVM CR pin MSRs
    Strengthen existing control register pinning when running
    paravirtualized under KVM. Check which bits KVM supports pinning for
    each control register and only pin supported bits which are already
    pinned via the existing native protection. Write to KVM CR0/4 pinned
    MSRs to enable pinning.
    
    Initiate KVM assisted pinning directly following the setup of native
    pinning on boot CPU. For non-boot CPUs initiate paravirtualized pinning
    on CPU identification.
    
    Identification of non-boot CPUs takes place after the boot CPU has setup
    native CR pinning. Therefore, non-boot CPUs access pinned bits setup by
    the boot CPU and request that those be pinned. All CPUs request
    paravirtualized pinning of the same bits which are already pinned
    natively.
    
    Guests using the kexec system call currently do not support
    paravirtualized control register pinning. This is due to early boot
    code writing known good values to control registers, these values do
    not contain the protected bits. This is due to CPU feature
    identification being done at a later time, when the kernel properly
    checks if it can enable protections. As such, the pv_cr_pin command line
    option has been added which instructs the kernel to disable kexec in
    favor of enabling paravirtualized control register pinning. crashkernel
    is also disabled when the pv_cr_pin parameter is specified due to its
    reliance on kexec.
    
    When we fix kexec, we will still need a way for a kernel with support to
    know if the kernel it is attempting to load has support. If a kernel
    with this enabled attempts to kexec a kernel where this is not
    supported, it would trigger a fault almost immediately.
    
    Liran suggested adding a section to the built image acting as a flag to
    signify support for being kexec'd by a kernel with pinning enabled.
    Should that approach be implemented, it is likely that the command line
    flag (pv_cr_pin) would still be desired for some deprecation period. We
    wouldn't want the default behavior to change from being able to kexec
    older kernels to not being able to, as this might break some users
    workflows.
    
    Signed-off-by: John Andersen <john.s.andersen@intel.com>

--- arch/x86/include/asm/kvm_para.h
+++ arch/x86/include/asm/kvm_para.h
@@ -112,6 +112,23 @@ static inline void kvm_spinlock_init(void)
 }
 #endif /* CONFIG_PARAVIRT_SPINLOCKS */
 
+#ifdef CONFIG_PARAVIRT_CR_PIN
+void __init kvm_paravirt_cr_pinning_init(void);
+void kvm_setup_paravirt_cr_pinning(unsigned long cr0_pinned_bits,
+				   unsigned long cr4_pinned_bits);
+#else
+static inline void kvm_paravirt_cr_pinning_init(void)
+{
+	return;
+}
+
+static inline void kvm_setup_paravirt_cr_pinning(unsigned long cr0_pinned_bits,
+						 unsigned long cr4_pinned_bits)
+{
+	return;
+}
+#endif /* CONFIG_PARAVIRT_CR_PIN */
+
 #else /* CONFIG_KVM_GUEST */
 #define kvm_async_pf_task_wait_schedule(T) do {} while(0)
 #define kvm_async_pf_task_wake(T) do {} while(0)
@@ -144,6 +161,17 @@ static inline void kvm_disable_steal_time(void)
 static inline bool kvm_handle_async_pf(struct pt_regs *regs, u32 token)
 {
 	return false;
+}
+
+static inline void kvm_paravirt_cr_pinning_init(void)
+{
+	return;
+}
+
+static inline void kvm_setup_paravirt_cr_pinning(unsigned long cr0_pinned_bits,
+						 unsigned long cr4_pinned_bits)
+{
+	return;
 }
 #endif
 
